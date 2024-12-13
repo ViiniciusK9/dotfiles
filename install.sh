7002736b43c18af2673167248d15c31db4508357
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-12-13 15:02:55 -0300

    remove vim powerline

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 # Fail on any command.
 set -eux pipefail
 
-# Install Powerline for VIM.
-sudo apt install -y python3-pip
-pip3 install --user powerline-status
-sudo cp configs/.vimrc ~/.vimrc
-sudo apt install -y fonts-powerline
-
 # Install Patched Font
 mkdir ~/.fonts
 sudo cp -a fonts/. ~/.fonts/
